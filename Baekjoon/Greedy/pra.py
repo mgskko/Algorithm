8f0e73245d0fe40a92b94b22bccf805e9f3fe58a
mgskko <msk724012@gmail.com>
2023-02-09 19:19:03 +0900

    0209 _ 브루트포스 추가

--- Baekjoon/Greedy/pra.py
+++ Baekjoon/Greedy/pra.py
@@ -1,2 +1,8 @@
 n = int(input())
-l = list(map(int,input().split()))
+x = list(map(int,input().split()))
+t = 0
+result = []
+x.sort(reverse=True)
+for i in range(n):
+  result.append(x[i] + i)
+print(max(result) + 2)
