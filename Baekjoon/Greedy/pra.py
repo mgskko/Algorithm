f446858a4fa33f86749bca6bb28b33f6d9830156
mgskko <msk724012@gmail.com>
2023-02-10 05:41:44 +0900

    0210

--- Baekjoon/Greedy/pra.py
+++ Baekjoon/Greedy/pra.py
@@ -1,8 +1,5 @@
-n = int(input())
-x = list(map(int,input().split()))
-t = 0
-result = []
-x.sort(reverse=True)
-for i in range(n):
-  result.append(x[i] + i)
-print(max(result) + 2)
+# %%
+print(1)
+# %%
+print(-1)
+# %%
