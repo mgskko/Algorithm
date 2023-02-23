333a1899d6460faa992780f047066a74e18fefc9
mgskko <msk724012@gmail.com>
2023-02-23 21:59:20 +0900

    0223

--- pra.py
+++ pra.py
@@ -1,12 +1,1 @@
-import sys
-
-n = int(sys.stdin.readline())
-k = list(map(int, sys.stdin.readline().split()))
-m = []
-
-for i in k:
-    a = 0
-    for j in k:
-        a += abs(i - j)
-    m.append(a)
-print(min(m))
+print(162//2)
