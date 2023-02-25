7e36624cdc87b23c5f496429f8fb6e9dd90ee6af
mgskko <msk724012@gmail.com>
2023-02-25 23:38:09 +0900

    0225

--- Baekjoon/Greedy/pra.py
+++ Baekjoon/Greedy/pra.py
@@ -1,1 +1,15 @@
-print('d')
+n, m = map(int, input().split())
+a = set()
+
+for i in range(n):
+    a.add(input())
+
+b = set()
+for i in range(m):
+    b.add(input())
+
+r  = sorted(list(a & b))
+print(len(r))
+
+for i in r:
+    print(i)
--- Baekjoon/Sorting/pra.py
+++ Baekjoon/Sorting/pra.py
@@ -1,19 +1,7 @@
-import sys
+n = input()
+a = []
 
-n = int(sys.stdin.readline())
-a = list(map(int,sys.stdin.readline().split()))
-k = int(sys.stdin.readline())
-cnt = 0
-left = 0
-right = n - 1
-a.sort()
-
-while left != right:
-  if a[left] + a[right] == k:
-    cnt += 1
-    left += 1
-  elif a[left] + a[right] > k:
-    right -= 1
-  else:
-    left += 1
-print(cnt)
+for i in range(len(n)):
+    a.append(n[i])
+a.sort(reverse=True)
+print(''.join(a))
