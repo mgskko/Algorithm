ddd9ed507d025b1790e4359ea406fe636d58dae0
mgskko <msk724012@gmail.com>
2023-02-28 14:02:31 +0900

    0228

--- pra.py
+++ pra.py
@@ -1,1 +1,12 @@
-print(162//2)
+n = int(input())
+
+m = []
+for i in range(n):
+    m.append(input().split())
+
+m.sort(key=lambda x:int(x[0]))
+m.sort(key=lambda x:x[1])
+
+for result in m:
+  print(result[0], result[1])
+
