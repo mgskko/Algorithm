660894ab158063c9c7ab9298d014d6b7460e451d
mgskko <msk724012@gmail.com>
2023-03-23 17:01:23 +0900

    0323

--- pra.py
+++ pra.py
@@ -1,24 +1,17 @@
-import sys
-input = sys.stdin.readline
 n = int(input())
-dp = [0] * (n+1)
-
-dp[1] = 1
-dp[2] = 2
-for i in range(3,n+1):
-    dp[i] = dp[i-1] +dp[i-2]
-if n == 1:
-    print(1)
-elif n == 2:
-    print(2)
-else:
-    print(dp[n]%10007)
-
-n = int(input())
-dp = [0]*1001
-dp[1] = 1
-dp[2] = 2
-
-for i in range(3,1001):
-    dp[i] = dp[i-1] + dp[i-2]
-print(dp[n]%10007)
+for _ in range(n):
+    k = int(input())
+    if k == 1:
+        print(1)
+    elif k == 2:
+        print(2)
+    elif k == 3:
+        print(4)
+    else:
+        dp = [0] * 11
+        dp[1] = 1
+        dp[2] = 2
+        dp[3] = 4
+        for i in range(4,11):
+            dp[i] = dp[i-1] + dp[i-2] + dp[i-3]
+    print(dp[k])
