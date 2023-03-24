01e3c51d03fe49b87b79d67550aaad356c5150b9
mgskko <msk724012@gmail.com>
2023-03-24 18:24:06 +0900

    0324

--- pra.py
+++ pra.py
@@ -1,17 +1,15 @@
 n = int(input())
-for _ in range(n):
-    k = int(input())
-    if k == 1:
-        print(1)
-    elif k == 2:
-        print(2)
-    elif k == 3:
-        print(4)
-    else:
-        dp = [0] * 11
-        dp[1] = 1
-        dp[2] = 2
-        dp[3] = 4
-        for i in range(4,11):
-            dp[i] = dp[i-1] + dp[i-2] + dp[i-3]
-    print(dp[k])
+s = [0 for i in range(301)]
+dp = [0 for i in range(301)]
+
+for i in range(1, n+1):
+    s[i] = int(input())
+
+dp[1] = s[1]
+dp[2] = s[1] + s[2]
+dp[3] = max(s[2]+s[3],s[1]+s[3])
+
+for i in range(4,n+1):
+    dp[i] = max(s[i] + s[i-1] + dp[i-3], s[i] + dp[i-2])
+
+print(dp[n])
