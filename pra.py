2e916dc8bb6be1aefc005ee3d48e424bab581e44
mgskko <msk724012@gmail.com>
2023-03-31 16:23:53 +0900

    0331

--- pra.py
+++ pra.py
@@ -1,12 +1,1 @@
-n = int(input())
-p = list([int(input()) for i in range(n)])
-p.reverse()
-cnt = 0
-for i in range(1,n):
-    if p[i] >= p[i-1]:
-        dif = p[i-1] - 1
-        cnt += p[i] - dif
-        p[i] = dif
-        dif = 0
-    
-print(cnt)
+print(int(input()))
