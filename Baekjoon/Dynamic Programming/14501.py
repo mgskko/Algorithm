29bff510ebde0d1ea0d0f29df6886c0e32dcd34f
mgskko <msk724012@gmail.com>
2023-04-11 18:41:40 +0900

    0401

--- Baekjoon/Dynamic Programming/14501.py
+++ Baekjoon/Dynamic Programming/14501.py
@@ -1,13 +1,19 @@
 import sys
 input = sys.stdin.readline
 
+# 입력 받기
 n = int(input())
-k = [list((map(int,input().split()))) for _ in range(n)]
-dp = [0]*(n+1)
+k = [list(map(int,input().split())) for _ in range(n)]
 
+# dp 테이블 초기화
+dp = [0] *(n+1)
+
+# 뒤에서부터 dp 진행
 for i in range(n-1,-1,-1):
-    if k[i][0] + i > n:
+    if i + k[i][0] > n: # 인덱스 범위를 초과하는 경우, 현재 위치의 값은 다음 값으로 업데이트
         dp[i] = dp[i+1]
-    else:
+    else: # 인덱스 범위 내에 있는 경우, 뒤에 있는 값들과 현재 위치의 값을 비교해서 최대값을 저장
         dp[i] = max(dp[i+1], k[i][1] + dp[i + k[i][0]])
+
+# dp 테이블에서 최대값 출력
 print(max(dp))
--- pra.py
+++ pra.py
@@ -2,19 +2,13 @@ import sys
 input = sys.stdin.readline
 
 n = int(input())
-dp = [[0 for _ in range(10)] for _ in range(n+1)]
-for i in range(1,10):
-    dp[1][i] = 1
+k = [list(map(int,input().split())) for _ in range(n)]
+dp = [0] *(n+1)
 
-MOD = 1000000000
+for i in range(n-1,-1,-1):
+    if i + k[i][0]> n:
+        dp[i] = dp[i+1]
+    else:
+        dp[i] = max(dp[i+1], k[i][1] + dp[i + k[i][0]])
 
-for i in range(2,n+1):
-    for j in range(10):
-        if j == 0:
-            dp[i][j] = dp[i-1][j+1]
-        elif j == 9:
-            dp[i][j] = dp[i-1][j-1]
-        else:
-            dp[i][j] = dp[i-1][j-1] + dp[i-1][j+1]
-
-print(sum(dp[n])% MOD)
+print(max(dp))
