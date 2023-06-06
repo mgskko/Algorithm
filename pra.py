fa0dcb4e923f6f602e06e5e6dfe52c7c526b7de2
mgskko <msk724012@gmail.com>
2023-06-06 22:37:33 +0900

    0606

--- pra.py
+++ pra.py
@@ -1,15 +1,25 @@
 import sys
 input = sys.stdin.readline
+
+# 입력을 받아옵니다.
 n = int(input())
-t = list(map(int, input().split()))
-t.sort()
-cnt = 0
-k = []
-if (len(t) % 2) == 1:
-    k.append(t.pop(-1))
-for i in range(len(t)):
-    if t[i] > t[(len(t)-i-1)]:
-        break
-    k.append(t[i]+t[(len(t)-i-1)])
-    
-print(max(k))
+
+# 입력된 값들을 저장할 리스트를 생성합니다.
+s = []
+
+# 결과를 저장할 변수를 초기화합니다.
+t = 0
+
+# n번 반복하면서 입력값을 리스트에 추가합니다.
+for i in range(n):
+    s.append(int(input()))
+
+# 입력값들을 오름차순으로 정렬합니다.
+s.sort()
+
+# 1부터 n까지 반복하면서 각 값과 해당 인덱스의 차이의 절댓값을 결과 변수에 누적합니다.
+for i in range(1, n+1):
+    t += abs(i - s[i-1])
+
+# 결과를 출력합니다.
+print(t)
