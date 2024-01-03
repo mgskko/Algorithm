4778d58a595b0431961e0486f91df344936b1b61
mgskko <msk724012@gmail.com>
2024-01-03 20:36:53 +0900

    0103

--- Baekjoon/Greedy/pra.py
+++ Baekjoon/Greedy/pra.py
@@ -1,19 +1,12 @@
-N = int(input())
+import sys
 
-dp = [[0]*10 for _ in range(N+1)]
-for i in range(1, 10):
-    dp[1][i] = 1
-
-MOD = 1000000000
-
-for i in range(2, N+1):
-    for j in range(10):
-        if j == 0:
-            dp[i][j] = dp[i-1][1]
-        elif j == 9:
-            dp[i][j] = dp[i-1][8]
-        else:
-            dp[i][j] = dp[i-1][j-1] + dp[i-1][j+1] 
-
-print(sum(dp[N]) % MOD)
-
+n = int(sys.stdin.readline())
+sum = 0
+result = 0
+for i in range(1, n + 1):
+    sum += i
+    result += 1
+    if sum > n:
+        result -= 1
+        break
+print(result)
