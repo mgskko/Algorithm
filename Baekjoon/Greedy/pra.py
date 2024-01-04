465d0d56b37de620ff31b5a281e1f1ad21f8dc27
mgskko <msk724012@gmail.com>
2024-01-04 20:03:23 +0900

    0104

--- Baekjoon/Greedy/pra.py
+++ Baekjoon/Greedy/pra.py
@@ -1,12 +1,12 @@
 import sys
 
 n = int(sys.stdin.readline())
-sum = 0
-result = 0
-for i in range(1, n + 1):
-    sum += i
-    result += 1
-    if sum > n:
-        result -= 1
-        break
-print(result)
+M = input()
+
+count = int(M.count('LL'))
+
+if count <= 1:
+    print(len(M))
+else :
+    print(len(M) - count + 1)
+
